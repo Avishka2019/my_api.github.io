1a625e313934dacc1bacc40dba7be11355916e0b
Avishka2019 <avishkadevinda2017@gmail.com>
2020-07-23 11:19:28 +0530

    Updates

---
+++ precache-manifest.dfcf5de7177499d5b90b5f1659baaa65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833a45dac24c4b95e5baeda086a41cc6",
+    "url": "/aviblogger/index.html"
+  },
+  {
+    "revision": "7f4637d130cf041b7029",
+    "url": "/aviblogger/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f49374d9f263d7990c9b",
+    "url": "/aviblogger/static/js/2.7ae12c9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aviblogger/static/js/2.7ae12c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f4637d130cf041b7029",
+    "url": "/aviblogger/static/js/main.e1a37fca.chunk.js"
+  },
+  {
+    "revision": "91e0f467c2fba33af45f",
+    "url": "/aviblogger/static/js/runtime-main.6cdb12d1.js"
+  }
+]);
